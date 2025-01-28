78983bf4b254338747b68b548b3c2ddbb71cb61d
jmoncayo-pursuit <jmoncayo@pursuit.org>
2025-01-28 10:27:40 -0500

    feat: implement string manipulation functions
    - Implement  to reverse a given string without using built-in methods.
    - Implement  to capitalize the first letter of each word without using built-in methods.
    - Implement  to count the number of vowels in a string without using built-in methods.

--- lesson05-interview-process/warmup.js
+++ lesson05-interview-process/warmup.js
@@ -1,9 +1,34 @@
-function reverseString(str) {}
+function reverseString(str) {
+let reversed = '';
+    for (let i = str.length - 1; i >= 0;i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
 
 console.log(reverseString("Hello, World!"));
 // Expected Output: "!dlroW ,olleH"
 
-function capitalizeWords(str) {}
+function capitalizeWords(str) {
+    let result = '';
+    let capitilizeNext = true;
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        if (char === ' ') {
+            result += char;
+            capitilizeNext = true;
+        } else {
+            if (capitilizeNext){ 
+                result += char.toUpperCase();
+                capitilizeNext = false;
+            } else {
+                result += char;
+            }
+        }
+    }
+    return result;
+}
 
 console.log(capitalizeWords("hello, world!"));
 // Expected Output: 'Hello, World!'
@@ -11,7 +36,16 @@ console.log(capitalizeWords("hello, world!"));
 console.log(capitalizeWords("javascript is awesome"));
 // Expected Output: 'Javascript Is Awesome'
 
-function countVowels(str) {}
+function countVowels(str) {
+    let count = 0;
+    const vowels = 'aeiouAEIOU';
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.indexOf(str[i]) !== -1) {
+            count++;
+        }
+    }
+    return count;
+}
 
 console.log(countVowels("hello"));
 // Expected Output: 2
